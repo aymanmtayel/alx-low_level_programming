bfe41829bf3374800c6608896cbbe25ffe20f8d6
aymanmtayel <eng.aymantayel@gmail.com>
2023-02-16 12:58:20 -0800

    puts trial 100000

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-/*this main to print a simple message*/
-/*always return 0*/
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
