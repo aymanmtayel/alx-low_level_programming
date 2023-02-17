d1f36ce928e63fc38bc695b703914a23315db42f
aymanmtayel <eng.aymantayel@gmail.com>
2023-02-17 12:09:45 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - print all single digit numbers of base 10
+ *starting from 0
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+int n;
+char l;
+for (n = 0 ; n <= 9 ; n++)
+{
+putchar((n % 10) + '0');
+}
+for (l = 'a'; l <= 'f' ; l++)
+{
+putchar(l);
+ }
+putchar('\n');
+return (0);
+ }
