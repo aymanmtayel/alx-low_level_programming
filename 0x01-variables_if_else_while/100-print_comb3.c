2e98d645aa5884479437b48285729b1cf10c3cc8
aymanmtayel <eng.aymantayel@gmail.com>
2023-02-17 12:53:06 -0800

    ASCIIIIII

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,32 +8,28 @@
 
 int main(void)
 {
-        int a;
-        int b;  
+int a;
+int b;  
 
-	a = 48;
-	b = 48;
+a = 48;
+b = 48;
 
-	while (a < 58)
-	{
-		b = a + 1;
-		while (b < 58)
-		{
-			putchar(a);
-			putchar(b);
-
-			if (a < 56 || b < 57)
-			{
-				putchar(44);
-				putchar(32);
-			}
-			b++;
-		}
-
-		a++;
-	}
-
-	putchar(10);
-
-	return (0);
+while (a < 58)
+{
+b = a + 1;
+while (b < 58)
+{
+putchar(a);
+putchar(b);
+if (a < 56 || b < 57)
+{
+putchar(44);
+putchar(32);
+}
+b++;
+}
+a++;
+}
+putchar(10);
+return (0);
 }
