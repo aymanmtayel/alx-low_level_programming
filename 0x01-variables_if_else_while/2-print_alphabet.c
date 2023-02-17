26c109be513b31846f30902d4d209c3aa883a6a4
aymanmtayel <eng.aymantayel@gmail.com>
2023-02-17 10:47:29 -0800

    abcde"

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - produce the alphabet in lower case
+ *
+ *Return: Always  0
+ */
+int main(void)
+{
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+return (0);
+}
