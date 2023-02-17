9c9df879ec48d4efea5aa4270cd99fe48f0667c6
aymanmtayel <eng.aymantayel@gmail.com>
2023-02-17 11:30:50 -0800

    betty

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-  int n;
-  
-  for (n = 0;n<10;n++)
-    {
-      printf("%i", n);
-    }
-  printf("\n");
-  return (0);
+int n;
+
+for (n = 0;n<10;n++)
+{
+printf("%i", n);
+}
+printf("\n");
+return (0);
 }
