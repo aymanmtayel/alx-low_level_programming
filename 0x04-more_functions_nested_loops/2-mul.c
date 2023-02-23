27fddf86b39e0ec8b13594177355d8fcaf637d53
aymanmtayel <eng.aymantayel@gmail.com>
2023-02-23 12:49:55 -0800

    mully

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *mul - function to multiply two integers
+ *@a: first integer
+ *@b: second integer
+ *Return: integer a times b.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
