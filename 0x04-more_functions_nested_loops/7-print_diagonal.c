b53b5f79b953e5cff054f0904f572dd66f60b1f4
aymanmtayel <eng.aymantayel@gmail.com>
2023-02-23 15:16:50 -0800

    diagonalized

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,32 +1,32 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ *print_diagonal - print diagonals with spaces
+ *@n: is number of diagonals
+ *Return: void
  *
  */
 
 void print_diagonal(int n)
 {
-  int i = 0;
-  int k;
+int i = 0;
+int k;
 
-  if (n > 0)
-    {
-      while (i < n)
-	{
-  for (k=0;k<i;k++)
-    {
-      _putchar(32);
-    }
-  _putchar(92);
-  _putchar('\n');
-  i++;
+if (n > 0)
+{
+while (i < n)
+{
+for (k=0;k<i;k++)
+{
+_putchar(32);
+}
+_putchar(92);
+_putchar('\n');
+i++;
 }
 }
-  else
-    {
-      _putchar('\n');
-    }
+else
+{
+_putchar('\n');
+}
 }
