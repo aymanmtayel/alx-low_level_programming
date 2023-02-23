c707e0ca0d1783c6ab424581e45ffa0513fb689b
aymanmtayel <eng.aymantayel@gmail.com>
2023-02-23 10:43:35 -0800

    lines

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,7 +10,6 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
-
 i = 0;
 
 /*
@@ -19,8 +18,7 @@ i = 0;
  *     putchar(i);
  *	}
  */
-
+ 
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
