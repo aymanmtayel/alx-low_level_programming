dcf89c73df2bc319c952af608e48a3a320842311
aymanmtayel <eng.aymantayel@gmail.com>
2023-02-23 14:34:08 -0800

    print the lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_line - print lines as many as inputed
+ *@n: number of dashes needed
+ *Return: void
+ *
+ */
+
+void print_line(int n)
+{
+int k;
+while (k < n)
+{
+_putchar(45);
+k++;
+}
+_putchar('\n');
+k=0;
+}
