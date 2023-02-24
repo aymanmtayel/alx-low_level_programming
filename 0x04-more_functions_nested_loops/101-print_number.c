b0d570ded996834a642817692b847a30510a1aed
Ayman Tayel <eng.aymantayel@gmail.com>
2023-02-24 22:16:26 +0200

    advanced task2

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_number - print the numbers inputted
+ *@n: numbers inputted
+ *Renturn: void
+ *
+ */
+
+void print_number(int n)
+{
+int k = n;
+
+if (n < 0)
+{
+n *= -1;
+k = n;
+_putchar('-');
+}
+k /= 10;
+if (k != 0)
+print_number(k);
+_putchar(n % 10 + '0');
+}
