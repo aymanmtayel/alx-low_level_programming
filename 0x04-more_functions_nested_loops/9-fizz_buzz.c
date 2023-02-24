b38818ca7c7939bd68633e98f529d876f8ba3958
aymanmtayel <eng.aymantayel@gmail.com>
2023-02-23 16:10:39 -0800

    fix count

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,16 @@ int i;
 for (i = 1; i < 101; i++)
 {
 if ((i % 3 == 0) && (i % 5 == 0))
-printf("%s ", "FizzBuzz");
+printf("FizzBuzz ");
 else if (i % 3 == 0)
-printf("%s ", "Fizz");
+printf("Fizz ");
 else if (i % 5 == 0)
-printf("%s ", "Buzz");
+{
+if (i == 100)
+printf("Buzz");
+else
+printf("Buzz ");
+}
 else
 printf("%i ", i);
 }
