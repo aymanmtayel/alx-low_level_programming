9109f77737b94dfcf327b3067444597a8f5b626d
aymanmtayel <eng.aymantayel@gmail.com>
2023-02-27 14:07:08 -0800

    task 9 fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,15 +6,17 @@
  * @src: The string to copy
  * Return: A pointer to the destination string @dest.
  */
-char *_strcpy(char *dest, const char *src)
-{
-int i = 0;
 
-while (src[i])
+char *_strcpy(char *dest, char *src)
 {
-dest[i] = src[i];
-i++;
+char *start = dest;
+
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
 }
-dest[i] = '\0';
-return (dest);
+*dest = '\0';
+return (start);
 }
