a96e7d56335d8b0d552c95f40f5e707bac75b391
aymanmtayel <eng.aymantayel@gmail.com>
2023-02-27 13:04:53 -0800

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#incude "main.h"
+
+/**
+ * print_rev - gets a string and then revers it.
+ * @s: string.
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+;
+for (i = i - 1; s[i] != '\0'; i--)
+{
+_putchar(s[i]);
+}
+}
