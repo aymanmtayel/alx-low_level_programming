6bc48cb458ec25100786eedadd5f41eb66ae35cb
Ayman Tayel <eng.aymantayel@gmail.com>
2023-03-08 22:49:23 +0200

    task03 hot fix

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,21 +8,17 @@
 
 int factorial(int n)
 {
-if (n <= 0)
-{
-return (-1);
-}
-else if (n == 0)
-{
-return (1);
-}
-else if (n > 0)
+
+if (n > 0)
 {
 return (n * factorial(n - 1));
+}
+else if (n < 0)
+{
+return (-1);
 }
 else
 {
 return (1);
 }
 }
-
