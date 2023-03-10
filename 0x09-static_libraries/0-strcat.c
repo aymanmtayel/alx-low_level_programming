fb9766a71ce544093e242d811bfd54a0370cc40f
Ayman Tayel <eng.aymantayel@gmail.com>
2023-03-10 23:01:24 +0200

    fix one of the 20

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,1 +1,25 @@
 #include "main.h"
+/**
+ * _strcat - concatenates two strings.
+ * @dest: first string
+ * @src: the string to append
+ * Return: the full string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int l, i;
+
+l = 0;
+while (dest[l] != '\0')
+{
+l++;
+}
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[l] = src[i];
+l++;
+}
+dest[l] = '\0';
+return (dest);
+}
