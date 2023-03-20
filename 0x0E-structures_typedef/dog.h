550fd9becfa33dbf11b0ee8812b8cc8d265bd713
aymanmtayel <eng.aymantayel@gmail.com>
2023-03-20 14:25:21 -0700

    put the typedef in the correct line

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,8 +8,6 @@
  *@owner: owner of the dog
  */
 
-typedef struct dog dog_t;
-
 struct dog
 {
 char *name;
@@ -19,4 +17,7 @@ char *owner;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
 #endif
