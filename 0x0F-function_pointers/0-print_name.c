3fc11737bcdec79cba3884319d5665b89bde1b18
aymanmtayel <eng.aymantayel@gmail.com>
2023-03-22 12:07:06 -0700

    task 00

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - print a name
+ *@name: the name to be printed
+ *@f:pointer to the function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
