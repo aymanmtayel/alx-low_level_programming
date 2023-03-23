03ab5cad9714fb76428081770955bb7da5f49934
aymanmtayel <eng.aymantayel@gmail.com>
2023-03-23 15:35:38 -0700

    task01

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,10 @@
 #ifndef VF
 #define VF
 #include <stdarg.h>
+#include <stddef.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
