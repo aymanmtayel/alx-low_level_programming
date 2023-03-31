b262ae72c2d7e69e167e82ab424d3a6b416e1d66
aymanmtayel <eng.aymantayel@gmail.com>
2023-03-30 19:56:34 -0700

    betty fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 /**
- * print_list - print all the elements of a list_t
- * @h: pointer to the head of the list
- * Return : the number of nodes
- */
+ *print_list - print all the elements of a list_t
+ *@h: pointer to the head of the list
+ *
+ *Return: the number of nodes
+*/
 
 size_t print_list(const list_t *h)
 {
