c6924c35957e5bc04b1e3b031b6ed117ccff394f
aymanmtayel <eng.aymantayel@gmail.com>
2023-05-08 17:00:45 -0700

    task3

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,87 +1,124 @@
 #include "main.h"
 
-/**
- * print_error - print an error message to stderr
- * @msg: the error message to print
- */
-void print_error(const char *msg)
-{
-	fprintf(stderr, "Error: %s\n", msg);
-	exit(98);
+#define BUF_SIZE 1024
+
+void print_error(const char* message, const char* filename);
+
+void print_header(const char* filename, char* buf);
+
+int main(int argc, char* argv[]) {
+	    int fd;
+	        char buf[BUF_SIZE];
+
+		    /* Check command line arguments */
+		    if (argc < 2) {
+			            print_error("Missing filename", "No file specified");
+				        }
+
+		        /* Open ELF file */
+		        fd = open(argv[1], O_RDONLY);
+			    if (fd < 0) {
+				            print_error("Cannot open file", argv[1]);
+					        }
+
+			        /* Read ELF header */
+			        if (read(fd, buf, BUF_SIZE) < 0) {
+					        print_error("Cannot read file", argv[1]);
+						    }
+
+				    /* Print ELF header */
+				    print_header(argv[1], buf);
+
+				        /* Close file and exit */
+				        close(fd);
+					    return 0;
 }
 
-/**
- * print_elf_header - print the information contained in the ELF header
- * @ehdr: a pointer to the ELF header structure
- */
-void print_elf_header(const Elf64_Ehdr *ehdr)
-{
-	int i;
-
-	printf("ELF Header:\n");
-	printf("  Magic:   ");
-	for (i = 0; i < EI_NIDENT; i++)
-	{
-		printf("%02x ", ehdr->e_ident[i]);
-	}
-	printf("\n");
-	printf("  Class:                             %s\n",
-	       ehdr->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-	printf("  Data:                              %s\n",
-	       ehdr->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
-	       "2's complement, big endian");
-	printf("  Version:                           %d%s\n",
-	       ehdr->e_ident[EI_VERSION], ehdr->e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
-	printf("  OS/ABI:                            %d\n", ehdr->e_ident[EI_OSABI]);
-	printf("  ABI Version:                       %d\n", ehdr->e_ident[EI_ABIVERSION]);
-	printf("  Type:                              %d\n", ehdr->e_type);
-	printf("  Entry point address:               0x%lx\n", ehdr->e_entry);
+void print_error(const char* message, const char* filename) {
+	    fprintf(stderr, "readelf: Error: %s: %s\n", message, filename);
+	        exit(98);
 }
 
-/**
- * main - entry point for the program
- * @argc: the number of command-line arguments
- * @argv: an array of pointers to the command-line arguments
- * Return: 0 on success, EXIT_FAILURE on failure
- */
-int main(int argc, char *argv[])
-{
-	int fd;
-	Elf64_Ehdr ehdr;
-	ssize_t n;
+void print_header(const char* filename, char* buf) {
+	    int i;
+	        uint16_t u16;
+		    uint32_t u32;
+		        uint64_t u64;
 
-	if (argc != 2)
-	{
-		print_error("Usage: elf_header elf_filename");
-	}
+			    /* Check ELF magic number */
+			    if (buf[0] != 0x7f || buf[1] != 'E' || buf[2] != 'L' || buf[3] != 'F') {
+				            print_error("File format not recognized", filename);
+					        }
 
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-	{
-		print_error("Failed to open file");
-	}
+			        /* Parse ELF header */
+			        printf("ELF Header:\n");
 
-	n = read(fd, &ehdr, sizeof(ehdr));
-	if (n == -1)
-	{
-		print_error("Failed to read ELF header");
-		close(fd);
-	}
-	if (n != sizeof(ehdr))
-	{
-		print_error("Incomplete ELF header");
-		close(fd);
-	}
+				    /* Magic number */
+				    printf("  Magic:   ");
+				        for (i = 0; i < 16; i++) {
+						        printf("%02x ", (unsigned char)buf[i]);
+							    }
+					    printf("\n");
 
-	if (ehdr.e_ident[EI_MAG0] != ELFMAG0 || ehdr.e_ident[EI_MAG1] != ELFMAG1 ||
-	    ehdr.e_ident[EI_MAG2] != ELFMAG2 || ehdr.e_ident[EI_MAG3] != ELFMAG3)
-	{
-		print_error("Not an ELF file");
-		close(fd);
-	}
+					        /* Class */
+					        printf("  Class:                             %s\n", buf[4] == 1 ? "ELF32" : buf[4] == 2 ? "ELF64" : "Invalid class");
 
-	print_elf_header(&ehdr);
+						    /* Data encoding */
+						    printf("  Data:                              %s\n", buf[5] == 1 ? "2's complement, little endian" : buf[5] == 2 ? "2's complement, big endian" : "Invalid data encoding");
 
-	close(fd);
-	return (EXIT_SUCCESS);
+						        /* Version */
+						        printf("  Version:                           %d%s\n", buf[6], buf[6] == 1 ? " (current)" : "");
+
+							    /* OS/ABI */
+							    printf("  OS/ABI:                            %s\n", buf[7] == 0 ? "UNIX - System V" : buf[7] == 3 ? "Linux" : "Other");
+
+							        /* ABI version */
+							        printf("  ABI Version:                       %d\n", buf[8]);
+
+								    /* Type */
+								    printf("  Type:                              %s\n", buf[16] == 1 ? "REL (Relocatable file)" : buf[16] == 2 ? "EXEC (Executable file)" : buf[16] == 3 ? "DYN (Shared object file)" : "Other");
+
+								        /* Machine */
+								        printf("  Machine:                           %s\n", buf[18] == 3 ? "x86" : buf[18] == 62 ? "x86-64" : "Other");
+
+									    /* Entry point */
+									    u64 = *(uint64_t*)(buf + 24 + (buf[4] == 2 ? 8 : 0));
+									        printf("  Entry point address:               0x%" PRIx64 "\n", u64);
+
+										    /* Program header offset */
+										    u64 = *(uint64_t*)(buf + 32 + (buf[4] == 2 ? 8 : 0));
+										        printf("  Start of program headers:          %lu (bytes into file)\n", u64);
+
+											    /* Section header offset */
+											    u64 = *(uint64_t*)(buf + 40 + (buf[4] == 2 ? 8 : 0));
+											        printf("  Start of section headers:          %lu (bytes into file)\n", u64);
+
+												    /* Flags */
+												    u32 = *(uint32_t*)(buf + 52 + (buf[4] == 2 ? 8 : 0));
+												        printf("  Flags:                             0x%" PRIx32 "\n", u32);
+
+													    /* Header size */
+													    u16 = *(uint16_t*)(buf + 54 + (buf[4] == 2 ? 8 : 0));
+													        printf("  Size of this header:               %d (bytes)\n", u16);
+
+														    /* Program header size */
+														    u16 = *(uint16_t*)(buf + 56 + (buf[4] == 2 ? 8 : 0));
+														        printf("  Size of program headers:           %d (bytes)\n", u16);
+
+															    /* Number of program headers */
+															    u16 = *(uint16_t*)(buf + 58 + (buf[4] == 2 ? 8 : 0));
+															        printf("  Number of program headers:         %d\n", u16);
+
+																    /* Section header size */
+																    u16 = *(uint16_t*)(buf + 60 + (buf[4] == 2 ? 8 : 0));
+																        printf("  Size of section headers:           %d (bytes)\n", u16);
+
+																	    /* Number of section headers */
+																	    u16 = *(uint16_t*)(buf + 62 + (buf[4] == 2 ? 8 : 0));
+																	        printf("  Number of section headers:         %d\n", u16);
+
+																		    /* Section header string table index */
+																		    u16 = *(uint16_t*)(buf + 64 + (buf[4] == 2 ? 8 : 0));
+																		        printf("  Section header string table index: %d\n", u16);
 }
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,13 +11,14 @@
 #include <fcntl.h>
 #include <elf.h>
 #include <errno.h>
+#include <inttypes.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_error(const char *msg);
-void print_elf_header(const Elf64_Ehdr *ehdr);
+void print_error(const char *msg, const char *filename);
+void print_header(const char *filename, char *buf);
 
 
 #endif
