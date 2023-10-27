c1fed016a976df4906630257a5ff9fd2fa645195
Ayman Tayel <eng.aymantayel@gmail.com>
2023-10-27 13:43:06 +0200

    taks 01

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - function to print the number of arguments passed to it
+ *@argc: arguments count
+ *@argv: arguments stored
+ *Return: 0 on success
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n",argc);
+	return (0);
+}
