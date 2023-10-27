a8933b9148ca7280c380a6aa115230b737ccf42b
Ayman Tayel <eng.aymantayel@gmail.com>
2023-10-27 13:34:58 +0200

    task 00

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - function to print its name
+ *@argc: arguments count unused
+ *@argv: array of arguments
+ *Return: 0 on success
+ */
+
+int main(int __attribute__((__unused__))argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
