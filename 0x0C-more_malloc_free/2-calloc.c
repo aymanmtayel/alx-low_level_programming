38228311813d1e590d092f91f983904b5ab0d251
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-03 19:09:13 +0200

    task02 fix the zero bytes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,20 +3,24 @@
 /**
  * _calloc - function that allocates memory for an array.
  * @nmemb : number of elements in the array.
- * @size : size of the array.
+ * @size : size of each element in the array.
  * Return: pointer to the allocated memory or NULL if fail
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(sizeof(size) * nmemb);
+	ptr = malloc(size * nmemb);
 
 	if (ptr == NULL)
 		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		*(ptr + i) =0;
 	return (ptr);
 }
