4605f2376a057dcca1267d3e409480860c3785f3
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-07 00:27:14 +0200

    reduce functions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,16 +10,9 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d->name)
-			printf("Name: %s\n", d->name);
-		if (d->age)
-			printf("Age: %f\n", d->age);
-		if (d->owner)
-			printf("Owner: %s\n", d->owner);
-		if (!d->name)
-			printf("Name: %s\n", "(nil)");
-		if (!d->owner)
-			printf("Owner: %s\n", "(nil)");
+		printf("Name: %s\n", dog.name ? dog.name : "(nil)");
+		printf("Age: %f\n", dog.age);
+		printf("Owner: %s\n", dog.owner ? dog.owner : "(nil)");
 	}
 }
 
