39b8b25473c85f94c9c697992cb229b90a53f788
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-06 23:53:35 +0200

    task00

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this is for the structures project tasks
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - It is for a dog details.
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: owner's name.
+ *
+ * Description : a dog struct for the info and details of a dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
