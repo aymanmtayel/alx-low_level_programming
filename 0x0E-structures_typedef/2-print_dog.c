91fa060d499215d7966e92d06a272b1b42de6626
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-07 00:39:30 +0200

    task try 6

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (!(d->name))
-			prinf("Name: (nil)\n");
+			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
