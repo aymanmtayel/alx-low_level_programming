74e58ccb875fd90f80682c414b77a2294a2f5d7f
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-07 01:02:40 +0200

    free the dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dog - function that frees the dogs.
+ * @d: the dog struct to be freed
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d->age);
+		free(d);
+	}
+}
