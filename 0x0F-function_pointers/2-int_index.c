e9d8f99b83bcc4a5f93ae9ea5c6eb4a02ebb4298
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-08 23:47:38 +0200

    task02 fix inital condition

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,16 +13,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && size && cmp)
+	if (array == NULL || size < 1 || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
 	{
-		if (size <= 0)
-			return (-1);
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]))
-				return (i);
-		}
-		return (1);
+		if (cmp(array[i]))
+			return (i);
 	}
 	return (-1);
 }
