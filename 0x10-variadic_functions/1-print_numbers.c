c04c5f5826982fb3b1b7c3a8150d100d3bd8b9a6
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-10 00:06:28 +0200

    fix the spearator null condition

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	for (i = 0; i < n - 1; i++)
 	{
-		printf("%d%s", va_arg(list, int), separator);
+		if (separator != NULL)
+			printf("%d%s", va_arg(list, int), separator);
+		if (separator == NULL)
+			printf("%d", va_arg(list, int));
 	}
 	printf("%d\n", va_arg(list, int));
 
