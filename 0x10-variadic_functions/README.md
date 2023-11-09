ff7ecfcad93d0da695097359658ba3f1f029875b
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-09 23:49:41 +0200

    task00

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This is for the variadic function project tasks
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef V_H
+#define V_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
