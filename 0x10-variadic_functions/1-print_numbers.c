6f18de5be496dea1392111ae904884fcaea9f3fe
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-10 00:02:25 +0200

    task01

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers
+ * @separator: the string to be printed between numbers
+ * @n: number of parameters passed
+ * @...: the integers to be printed.
+ *
+ * Return: Void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+
+	va_start(list, n);
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d%s", va_arg(list, int), separator);
+	}
+	printf("%d\n", va_arg(list, int));
+
+	va_end(list);
+}
