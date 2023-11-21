c6b79a64f86f63250258182cc00933e452ac3446
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-21 22:17:57 +0200

    task00

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a list
+ * @h: pointer to the start of the list
+ *
+ * Return: the number of the nodes on success
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i;
+
+	if (h == NULL)
+		return (0);
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is for the more singly linked lists project tasks
