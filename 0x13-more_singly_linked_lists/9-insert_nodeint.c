663de675a3934776de2c10edb34f0c72716da18c
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-22 00:47:48 +0200

    fix index count

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 	current = *head;
-	for (; current != NULL && i < idx; i++)
+	for (; current != NULL && i < idx - 1; i++)
 		current = current->next;
 	if (current == NULL)
 	{
