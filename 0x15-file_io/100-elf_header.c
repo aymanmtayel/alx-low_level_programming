b048c33b11e1d23c22699cf7f86eb0da1852ebf2
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-28 00:04:49 +0200

    fix code length

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,84 +1,68 @@
 #include "main.h"
 
 /**
- * p_error - prints error messages to standard error
- * @msg: the message
- * Return: Void
+ * print_elf_header - Prints information from the ELF header
+ * @elf_h: Pointer to the ELF header structure
  */
-void p_error(const char *msg)
-{
-	dprintf(STDERR_FILENO, "Error: %s\n", msg);
-}
-
-/**
- * print_elf_header - print the information contained in the ELF header
- * @ehdr: a pointer to the ELF header structure
- */
-
-void print_elf_header(Elf64_Ehdr *ehdr)
+void print_elf_header(Elf64_Ehdr *elf_h)
 {
 	int counter;
 
 	printf("  Magic:   ");
 	for (counter = 0; counter < EI_NIDENT; counter++)
-		printf("%02x ", ehdr->e_ident[counter]);
+		printf("%02x ", elf_h->e_ident[counter]);
 	printf("\n");
 
 	printf("  Class:   %s\n",
-	       (ehdr->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
+	       (elf_h->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
 	printf("  Data:    %s\n",
-	       (ehdr->e_ident[EI_DATA] == ELFDATA2LSB) ?
+	       (elf_h->e_ident[EI_DATA] == ELFDATA2LSB) ?
 	       "2's complement, little endian" : "2's complement, big endian");
-	printf("  Version: %d (current)\n", ehdr->e_ident[EI_VERSION]);
-	printf("  OS/ABI:  %d\n", ehdr->e_ident[EI_OSABI]);
-	printf("  ABI Ver: %d\n", ehdr->e_ident[EI_ABIVERSION]);
+	printf("  Version: %d (current)\n", elf_h->e_ident[EI_VERSION]);
+	printf("  OS/ABI:  %d\n", elf_h->e_ident[EI_OSABI]);
+	printf("  ABI Ver: %d\n", elf_h->e_ident[EI_ABIVERSION]);
 	printf("  Type:    %s\n",
-	       (ehdr->e_type == ET_EXEC) ? "EXEC (Executable file)" :
-	       (ehdr->e_type == ET_DYN) ? "DYN (Shared object file)" :
-	       (ehdr->e_type == ET_REL) ? "REL (Relocatable file)" : "Unknown");
-	printf("  Entry:   %lx\n", (unsigned long)ehdr->e_entry);
+	       (elf_h->e_type == ET_EXEC) ? "EXEC (Executable file)" :
+	       (elf_h->e_type == ET_DYN) ? "DYN (Shared object file)" :
+	       (elf_h->e_type == ET_REL) ? "REL (Relocatable file)" : "Unknown");
+	printf("  Entry:   %lx\n", (unsigned long)elf_h->e_entry);
 }
 
 /**
- * main - entry point for the program
- * @argc: the number of command-line arguments
- * @argv: an array of pointers to the command-line arguments
- * Return: 0 on success, EXIT_FAILURE on failure
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ * Return: 0 on success, 98 on failure
  */
 int main(int argc, char *argv[])
 {
-	int f_o;
-	Elf64_Ehdr ehdr;
-	ssize_t f_r;
+	int fo, m_check;
+	Elf64_Ehdr elf_h;
 
 	if (argc != 2)
-		p_error("Usage: elf_header elf_filename"), exit(98);
-	f_o = open(argv[1], O_RDONLY);
-	if (f_o == -1)
-		p_error("Failed to open file"), exit(98);
-	f_r = read(f_o, &ehdr, sizeof(ehdr));
-	if (f_r == -1)
-	{
-		p_error("Failed to read ELF header");
-		close(f_o);
-		return (EXIT_FAILURE);
-	}
-	if (f_r != sizeof(ehdr))
-	{
-		p_error("Incomplete ELF header");
-		close(f_o);
-		return (EXIT_FAILURE);
-	}
+		dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]), exit(98);
 
-	if (ehdr.e_ident[EI_MAG0] != ELFMAG0 || ehdr.e_ident[EI_MAG1] != ELFMAG1 ||
-	    ehdr.e_ident[EI_MAG2] != ELFMAG2 || ehdr.e_ident[EI_MAG3] != ELFMAG3)
-	{
-		p_error("Not an ELF file");
-		close(f_o);
-		return (EXIT_FAILURE);
-	}
+	fo = open(argv[1], O_RDONLY);
+	if (fo == -1)
+		perror("Error"), exit(98);
 
-	print_elf_header(&ehdr);
-	close(f_o);
-	return (EXIT_SUCCESS);
+	if (read(fo, &elf_h, sizeof(elf_h)) != sizeof(elf_h))
+		perror("Error"), close(fo), exit(98);
+	if (lseek(fo, 0, SEEK_SET) == -1)
+		perror("Error"), close(fo), exit(98);
+
+	m_check = (elf_h.e_ident[EI_MAG0] != ELFMAG0 ||
+		       elf_h.e_ident[EI_MAG1] != ELFMAG1 ||
+		       elf_h.e_ident[EI_MAG2] != ELFMAG2 ||
+		       elf_h.e_ident[EI_MAG3] != ELFMAG3);
+
+	if (m_check)
+		fprintf(stderr, "Error: Not an ELF file\n"), close(fo), exit(98);
+
+	printf("ELF Header:\n");
+	print_elf_header(&elf_h);
+
+	close(fo);
+	return (0);
 }
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,14 +9,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <elf.h>
-#include <errno.h>
+#include <stdint.h>
 
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void perror(const char *msg);
 void print_elf_header(Elf64_Ehdr *ehdr);
 
 
