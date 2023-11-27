4dd1349e0b470c0ac4bbd358dd00ed36bc671b78
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-27 23:10:32 +0200

    fix last error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,8 +9,7 @@
 
 int main(int argc, char **argv)
 {
-	int file1, file2;
-	ssize_t writef, readf;
+	int file1, file2, file_c;
 	char temp[1024];
 
 	if (argc != 3)
@@ -24,25 +23,25 @@ int main(int argc, char **argv)
 	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	while ((readf = read(file1, temp, 1024)) > 0)
+	while ((file_c = read(file1, temp, 1024)) > 0)
 	{
-		if (readf == -1)
+		if (file_c == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		writef = write(file2, temp, readf);
-		if (writef != readf)
+		file_c = write(file2, temp, file_c);
+		if (file_c == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-		if (writef == -1)
+	}
+		if (file_c == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-	}
-	if (close(file1) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1), exit(100);
-	if (close(file2) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2), exit(100);
-	return (0);
+		if (close(file1) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1), exit(100);
+		if (close(file2) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2), exit(100);
+		return (0);
 }
