3908307f74a363efb7200fe0d8a7f29766e07233
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-27 23:03:08 +0200

    fix errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,14 +9,12 @@
 
 int main(int argc, char **argv)
 {
-	int file1, file2, writef, readf;
+	int file1, file2;
+	ssize_t writef, readf;
 	char temp[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 == -1)
 	{
@@ -25,18 +23,17 @@ int main(int argc, char **argv)
 	}
 	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	while ((readf = read(file1, temp, 1024)) > 0)
 	{
 		if (readf == -1)
 		{
-			dprintf(STDERR_FILENO, "Eror: Can't read from file %s\n", argv[1]);
-			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
 		}
 		writef = write(file2, temp, readf);
+		if (writef != readf)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 		if (writef == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
