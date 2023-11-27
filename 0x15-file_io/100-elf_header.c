fc54cc5bc173a1dd2394e9b348467960d584cd73
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-27 23:46:08 +0200

    fix errors

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -7,35 +7,35 @@
  */
 void p_error(const char *msg)
 {
-	fprintf(stderr, "Error: %s\n", msg);
+	dprintf(STDERR_FILENO, "Error: %s\n", msg);
 }
 
 /**
  * print_elf_header - print the information contained in the ELF header
  * @ehdr: a pointer to the ELF header structure
  */
-void print_elf_header(const Elf64_Ehdr *ehdr)
-{
-	int counter;
 
-	printf("ELF Header:\n");
+void print_elf_header(Elf64_Ehdr *ehdr)
+{
+
 	printf("  Magic:   ");
-	for (counter = 0; counter < EI_NIDENT; counter++)
-	{
-		printf("%02x ", ehdr->e_ident[counter]);
-	}
+	for (int i = 0; i < EI_NIDENT; i++)
+		printf("%02x ", elf_header->e_ident[i]);
 	printf("\n");
-	printf("  Class:                             %s\n",
-	       ehdr->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-	printf("  Data:                              %s\n",
-	       ehdr->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
-	       "2's complement, big endian");
-	printf("  Version:                           %d%s\n",
-	       ehdr->e_ident[EI_VERSION], ehdr->e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
-	printf("  OS/ABI:                            %d\n", ehdr->e_ident[EI_OSABI]);
-	printf("  ABI Version:                       %d\n", ehdr->e_ident[EI_ABIVERSION]);
-	printf("  Type:                              %d\n", ehdr->e_type);
-	printf("  Entry point address:               0x%lx\n", ehdr->e_entry);
+
+	printf("  Class:   %s\n",
+	       (elf_header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
+	printf("  Data:    %s\n",
+	       (elf_header->e_ident[EI_DATA] == ELFDATA2LSB) ?
+	       "2's complement, little endian" : "2's complement, big endian");
+	printf("  Version: %d (current)\n", elf_header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:  %d\n", elf_header->e_ident[EI_OSABI]);
+	printf("  ABI Ver: %d\n", elf_header->e_ident[EI_ABIVERSION]);
+	printf("  Type:    %s\n",
+	       (elf_header->e_type == ET_EXEC) ? "EXEC (Executable file)" :
+	       (elf_header->e_type == ET_DYN) ? "DYN (Shared object file)" :
+	       (elf_header->e_type == ET_REL) ? "REL (Relocatable file)" : "Unknown");
+	printf("  Entry:   %lx\n", (unsigned long)elf_header->e_entry);
 }
 
 /**
@@ -51,16 +51,10 @@ int main(int argc, char *argv[])
 	ssize_t f_r;
 
 	if (argc != 2)
-	{
-		p_error("Usage: elf_header elf_filename");
-	       	return (EXIT_FAILURE);
-	}
+		p_error("Usage: elf_header elf_filename"), exit(98);
 	f_o = open(argv[1], O_RDONLY);
 	if (f_o == -1)
-	{
-		p_error("Failed to open file");
-		return (EXIT_FAILURE);
-	}
+		p_error("Failed to open file"), exit(98);
 	f_r = read(f_o, &ehdr, sizeof(ehdr));
 	if (f_r == -1)
 	{
