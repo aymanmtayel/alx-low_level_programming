417442abec8607f0d8f0ae3777f0ba470dfad2f5
Ayman Tayel <eng.aymantayel@gmail.com>
2023-11-27 22:46:53 +0200

    task03

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * main - program that copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: values of arguments
+ * Return: 0 if success, -1 if failed
+ */
+
+int main(int argc, char **argv)
+{
+	int file1, file2, writef, readf;
+	char temp[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file1 = open(argv[1], O_RDONLY);
+	if (file1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while ((readf = read(file1, temp, 1024)) > 0)
+	{
+		if (readf == -1)
+		{
+			dprintf(STDERR_FILENO, "Eror: Can't read from file %s\n", argv[1]);
+			exit(99);
+		}
+		writef = write(file2, temp, readf);
+		if (writef == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+	}
+	if (close(file1) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1), exit(100);
+	if (close(file2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2), exit(100);
+	return (0);
+}
