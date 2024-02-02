75c496a3a3d3920a0bc00b7a387d885fa0273ca7
Ayman Tayel <eng.aymantayel@gmail.com>
2024-02-02 16:32:04 +0200

    using calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,23 +9,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	unsigned long int i;
 
-	if (!size)
-		return (NULL);
 
-	table = malloc(sizeof(hash_table_t));
+	table = calloc(1, sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t *) * size);
+	table->array = calloc(size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
 		free(table);
+		free(table->array);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
-		table->array[i] = NULL;
-
 	return (table);
 }
