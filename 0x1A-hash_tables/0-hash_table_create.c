7aac50a3922b4e6fdbf53f63dad42f467b76de7f
Ayman Tayel <eng.aymantayel@gmail.com>
2024-02-02 16:16:26 +0200

    remove empty spaces

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,16 +14,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
-
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
-
 	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);
 	}
-
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 	return (table);
