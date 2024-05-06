f906be453756e38f07a649f19ecbb20b3e43a145
aymanmtayel <eng.aymantayel@gmail.com>
2024-05-06 22:04:20 +0300

    task00

---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+this is for the search algorithms project tasks
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef SearchA_H
+#define SearchA_H
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
