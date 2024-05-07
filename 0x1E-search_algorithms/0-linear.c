a18f095402c4d1d11a3cd11dcbdbf7f5d5d81c05
aymanmtayel <eng.aymantayel@gmail.com>
2024-05-07 14:40:42 +0300

    Task01 try2

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,14 +12,15 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL)
+	if (array == NULL || size == 0 || !value)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (value == array[i])
+		if (array[i] == value)
 			return (i);
 	}
+
 	return (-1);
 }
