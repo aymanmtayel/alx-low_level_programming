d117d6fc4cf60d11e8855774e75b85aa15fdd9a4
aymanmtayel <eng.aymantayel@gmail.com>
2024-05-07 14:25:21 +0300

    fix conditions

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,7 +13,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL)
+	if (array == NULL || value == NULL || size == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
